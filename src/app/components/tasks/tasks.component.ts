473cfbdd88d4d391168bcfc810907565f818d275
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-05-29 20:26:29 -0300

    feat: insert the data fecth

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private service: TaskService) {}
+
+  ngOnInit(): void {
+    this.service.getTasks().subscribe({
+      next: (res) => {
+        this.tasks = res;
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from '../models/task';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl: string = environment.TASK_API;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
 }
